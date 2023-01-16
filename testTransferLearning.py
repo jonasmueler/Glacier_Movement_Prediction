f0565b188edfaf9087dd25ed66f59fc1eecf6e00
jonasmueler <jonasmu@edu.aau.at>
2023-01-16 16:32:09 +0100

    test from pycharm

--- testTransferLearning.py
+++ testTransferLearning.py
@@ -2,13 +2,10 @@ import transferLearning
 import models
 import torch
 
-# create model
-test = [[torch.rand(5, 3, 50, 50, requires_grad=True),[torch.rand(1,3, requires_grad=True), torch.rand(1,3, requires_grad=True),torch.rand(1,3, requires_grad=True),torch.rand(1,3, requires_grad=True),torch.rand(1,3, requires_grad=True)]],
-        [torch.rand(5, 1, 50, 50, requires_grad=True),[torch.rand(1,3, requires_grad=True), torch.rand(1,3, requires_grad=True),torch.rand(1,3, requires_grad=True),torch.rand(1,3, requires_grad=True),torch.rand(1,3, requires_grad=True)]]]
-
-
 model = models.AE_Transformer(2420,2420, 2420, 3, 1, 1000, 6,1, True, None)
-r = model.forward(test, True)
-print(r)
+
+# test with 200*200 image
+testInput = torch.rand(5, 3, 200, 200, requires_grad=True)
+testTargets = torch.rand(5, 3, 200, 200, requires_grad=True)
 
 
