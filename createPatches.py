d979d2b8fc168425e6ccf5ceac4f7c30ec48a087
jonasmueler <jonasmu@edu.aau.at>
2023-02-07 18:08:18 +0100

    final dataset creation

--- createPatches.py
+++ createPatches.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 import pickle
 
 ########### first check images with plot, then save patched pickle files #######################
-year = "2013"
+year = "2021"
 
 path = "/media/jonas/B41ED7D91ED792AA/Arbeit_und_Studium/Kognitionswissenschaft/Semester_5/masterarbeit#/data_Code/datasets/Jungfrau_Aletsch_Bietschhorn"
 os.chdir(path)
 d = functions.loadData(path, [year])
 
+
+
 ## add ndsi masks
 d = functions.NDSI(d, threshold = 0.3)
 
-
+"""
 # find roi and good images
 for i in range(len(d)):# rgb
     img = functions.createImage(d[i][1][1:4,:,:], 0.4)
@@ -23,26 +25,28 @@ for i in range(len(d)):# rgb
 
 """
 # 2013
-#goodImg = [2, 3, 4, 5, 6, 7, 8, 10]
+#goodImg = [0, 3, 5, 6, 7]
 # 2014:
-#goodImg = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+#goodImg = [1, 2, 3, 4, 7, 8]
 # 2015:
-#goodImg = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+#goodImg = [1,2 ,3, 4, 5, 8, 9, 10, 12, 13, 14, 15]
 # 2016
-#goodImg = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23]
+#goodImg = [0, 1, 2, 3, 9, 10, 11, 12, 13]
 # 2017
-#goodImg = [0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+#goodImg = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 # 2018
-#goodImg = [0, 2, 3, 4, 7, 8, 9, 10, 11, 12]
+#goodImg = [2, 3, 4, 5, 10, 12, 13]
 # 2019
-#goodImg = [0, 2, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21]
+#goodImg = [2, 3, 5, 6, 11]
 # 2020
-#goodImg = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+#goodImg = [1, 2, 3, 4, 5, 7, 10]
 # 2021
-#goodImg = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+goodImg = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15]
+
+
 d = [d[i] for i in goodImg]
 
-d = functions.automatePatching(d, 50, 20, [100, 400, 200, 500], applyKernel=True)
+d = functions.automatePatching(d, 50, 40, [50, 650, 0, 600] , applyKernel=True)
 
 os.makedirs(path + "/patched", exist_ok = True)
 os.chdir(path + "/patched")
@@ -52,7 +56,7 @@ with open(year, "wb") as fp:  # Pickling
     pickle.dump(d, fp)
 print("data saved!")
 
-"""
+#"""
 
 
 
