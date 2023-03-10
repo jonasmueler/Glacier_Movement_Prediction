92df48899e757c756b97f693475c5d14cecc714f
jonasmueler <jonasmu@edu.aau.at>
2023-03-10 11:40:56 +0100

    new dataset class

---
+++ data_loader.py
@@ -0,0 +1,47 @@
+import os
+from torch.utils.data import Dataset
+from torch import is_tensor
+import functions
+
+class glaciers(Dataset):
+    def __init__(self, path, mode):
+        """
+        dataset class for train loop
+        path: str
+            path to image and target folder
+        """
+        self.path = path
+        self.mode = mode
+
+        # get list of all paths in directory
+        images = os.listdir(os.path.join(self.path, "images"))
+        paths = [os.path.join(images, item) for item in images]
+        self.images = paths
+
+        targets = os.listdir(os.path.join(self.path, "targets"))
+        paths = [os.path.join(images, item) for item in targets]
+        self.targets = paths
+
+    def __len__(self):
+        _, _, files = next(os.walk(os.path.join(self.path, "images"))))
+        fileCount = len(files)
+        return fileCount
+
+    def __getitem__(self, idx):
+        """
+        returns datum for training
+
+        idx: int
+            index to datum
+        returns: torch.tensor
+                image and targets
+        """
+        if is_tensor(idx):
+            idx = idx.tolist()
+
+        # get data in tensor format
+        inpt = functions.openData(self.images[idx])
+        target = functions.openData(self.targets[idx])
+
+        return inpt, target
+
